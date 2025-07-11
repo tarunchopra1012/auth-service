c975b688f1537fe7648a41f8996c463f481005b2
tarunchopra1012 <tarun.chopra10@gmail.com>
2025-07-11 16:34:50 +0530

    Remove dist folder from repository and add to .gitignore

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Basic TypeScript file to test compilation
-console.log('Hello TypeScript!');
-const test = {
-    message: 'TypeScript is working!'
-};
-console.log(test.message);
